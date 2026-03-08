cc63829eb4c2576505446118510319d3ec1b6f66
Pranav <jpranav1505>
2026-03-08 11:48:30 +0530

    Pranav added UC12 strategy pattern palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,15 +1,40 @@
-public class PalindromeService {
+import java.util.Scanner;
+
+/* Strategy Interface */
+interface PalindromeStrategy {
+    boolean isPalindrome(String word);
+}
+
+
+/* Reverse Strategy */
+class ReverseStrategy implements PalindromeStrategy {
 
     public boolean isPalindrome(String word) {
 
-        word = word.toLowerCase().replaceAll(" ", "");
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse += word.charAt(i);
+        }
+
+        return word.equals(reverse);
+    }
+}
+
+
+/* Array Strategy */
+class ArrayStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String word) {
+
+        char[] arr = word.toCharArray();
 
         int start = 0;
-        int end = word.length() - 1;
+        int end = arr.length - 1;
 
         while (start < end) {
 
-            if (word.charAt(start) != word.charAt(end)) {
+            if (arr[start] != arr[end]) {
                 return false;
             }
 
@@ -18,5 +43,57 @@ public class PalindromeService {
         }
 
         return true;
+    }
+}
+
+
+/* Stack Strategy */
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String word) {
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        String reverse = "";
+
+        while (!stack.isEmpty()) {
+            reverse += stack.pop();
+        }
+
+        return word.equals(reverse);
+    }
+}
+
+
+/* Main Class */
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter text:");
+        String word = sc.nextLine();
+
+        PalindromeStrategy strategy;
+
+        // choose strategy here
+        strategy = new ReverseStrategy();
+        // strategy = new ArrayStrategy();
+        // strategy = new StackStrategy();
+
+        boolean result = strategy.isPalindrome(word);
+
+        if (result) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
     }
 }
